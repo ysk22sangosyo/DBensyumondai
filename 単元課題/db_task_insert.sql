04d67e0861c858c81088d65175111ce27c65ad6a
ysk <22higa_yuusuke@axiz.co.jp>
2022-04-19 18:55:24 +0900

    hekou

--- 単元課題/db_task_insert.sql
+++ 単元課題/db_task_insert.sql
@@ -25,9 +25,31 @@ VALUES(101,'日本橋店','東京都中央区日本橋1-1-1','03-111-2222'),
 
 
 --支払方法出力
-insert into pay(pay_id,pay),
+insert into pay(pay_id,pay)
 VALUES(1,'現金'),(2, 'PayPay');
 
---顧客テーブル出力
+--カスタマー出力
 insert into custmer(custmer_id,custmer_name,custmer_old,custmer_sex,remarks1,remarks2,remarks3)
 VALUES(101,'山田　宏一',35,'男','TSUTAYA会員',null,null),(101,'比嘉　祐介',22,'男',null,null,null);
+
+--責任者出力
+insert into pay(responsible_no,manager_name,manager_old,manager_sex)
+VALUES(1,'菜綱　苗子',40,'女'),(20, 'まだ　義三',59,'男');
+
+--役職出力
+insert into position(position_id,position)
+VALUES(1,'社長'),(2,'副社長'),(3,'店長'),(4,'副店長'),(5,'正社員'),
+(6,'パート'),(7,'アルバイト');
+
+--会員情報出力
+insert into member(member_id,member_name)
+VALUES(1,'TSUTAYA'),(2,'LonelyMart');
+
+--支払内訳出力
+insert into breakdown(breakdown)
+VALUES(668),(1);
+
+--reciet出力
+insert into reciet(company_id,hidke,zikoku)
+VALUES(101,'2022年4月1日(金)','19:00'),
+(102,'2022年4月11日(月)','18:33');
